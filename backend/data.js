844c2f4556ada569afa988e76b9348c612833dda
pritesh-peter <pritesh.peter@gmail.com>
2021-05-25 18:06:49 +0545

    working in backend server

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "/images/product-1.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      countInStock: 10,
+      numReviews: 10,
+      description: "high quality product",
+    },
+
+    {
+      _id: "2",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "/images/product-1.jpg",
+      price: 122,
+      brand: "Adidas",
+      countInStock: 11,
+      rating: 2.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+
+    {
+      _id: "3",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "/images/product-1.jpg",
+      price: 120,
+      brand: "Nike",
+      countInStock: 0,
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+
+    {
+      _id: "4",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "/images/product-1.jpg",
+      price: 120,
+      brand: "Nike",
+      countInStock: 18,
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+
+    {
+      _id: "5",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "/images/product-1.jpg",
+      price: 120,
+      brand: "Nike",
+      countInStock: 6,
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+  ],
+};
+
+export default data;
